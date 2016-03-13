86f0768b7fd8a6ef16bf8e89e6a8f2be09b72eca
Steve (Home) <contact@stevemeredith.co.uk>
2016-03-13 03:44:13 +0000

    Added a box building mixin, some temporary art for textures and added a floor to the scene

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('compile-sass', function(){
-  return gulp.src('sass/dungeon.scss')
+  return gulp.src('sass/cssdungeon.scss')
     .pipe(sass())
     .pipe(gulp.dest('css'));
 })
