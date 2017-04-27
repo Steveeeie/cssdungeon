2b1c991e99f64b5ff24c506520a122e352d2cf25
Steveeeie <steveeeeie@gmail.com>
2017-04-27 23:03:58 +0100

    Added autoprefixer and set sass compiler to compress output

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
 
 gulp.task('compile-sass', function(){
   return gulp.src('sass/cssdungeon.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(rename({ suffix: ".min" }))
     .pipe(gulp.dest('css'))
     .pipe(browserSync.stream());
 })
